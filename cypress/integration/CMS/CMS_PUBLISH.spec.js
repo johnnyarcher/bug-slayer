862af68dd34474307a6d1222f8dbd5f0ff2a91a1
Johnny Archer <johnny.archer@getg5.com>
2020-09-03 11:45:37 -0700

    Lint

--- cypress/integration/CMS/CMS_PUBLISH.spec.js
+++ cypress/integration/CMS/CMS_PUBLISH.spec.js
@@ -1,5 +1,8 @@
 // <reference types="cypress" />
 describe("Bug-Slayer | CMS", () => {
   it("Visual Regression | Publish Before & After", () => { 
-  cy.visit(Cypress.env('STAGING_CLW'));
-  })
+  cy.visit('https://cms.g5marketingcloud.com');
+  cy.clearLocalStorage();
+  cy.get('#user_email').type(Cypress.env('GLOBAL_USER'));
+  cy.get('#user_password').type(Cypress.env('GLOBAL_PASSWORD'));
+  cy.get('.btn-primary').click();
